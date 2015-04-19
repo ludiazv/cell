0d140ecd9a840ec1ea8aeb589df31caa204f5c92
ludiazv <ldiaz@atloideas.com>
2015-04-19 11:13:54 +0200

    rails + changes

---
+++ jre7/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+source /etc/environment
+PREFIX="/"
+docker run -i --rm -t -e HOST_IP=${COREOS_PRIVATE_IPV4} -e CELL_ETCD_PREFIX=${PREFIX} --name jre7 atlo/jre7
--- ruby2/run.sh
+++ ruby2/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source /etc/environment
 PREFIX="/"
-docker run -i --rm -t -e HOST_IP=${COREOS_PRIVATE_IPV4} -e CELL_ETCD_PREFIX=${PREFIX} --name ruby2 atlo/ruby2 /bin/bash
+docker run -i --rm -t -e HOST_IP=${COREOS_PRIVATE_IPV4} -e CELL_ETCD_PREFIX=${PREFIX} --name ruby2 atlo/ruby2
