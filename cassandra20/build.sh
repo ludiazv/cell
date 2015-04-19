8f17b6548e13edb896c9ace230e7c5bc60a518f2
ludiazv <ldiaz@atloideas.com>
2015-04-19 13:57:43 +0200

    fix

--- cassandra20/build.sh
+++ cassandra20/build.sh
@@ -2,4 +2,4 @@
 VERSION=1.0
 QUIET=-q
 docker build $QUIET --force-rm=true --tag="atlo/cassandra20:$VERSION" .
-docker build $QUIET --force-rm=true --tag="atlo/cassandra20:latest"   .
+docker tag -f atlo/cassandra20:$VERSION atlo/cassandra20:latest
