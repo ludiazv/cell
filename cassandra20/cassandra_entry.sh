9090d5ab5be422c857bae20d1eca42979e01c0be
ludiazv <ldiaz@atloideas.com>
2015-04-23 19:20:51 +0200

    fix cassandra

--- cassandra20/cassandra_entry.sh
+++ cassandra20/cassandra_entry.sh
@@ -8,7 +8,7 @@ echo "$(date -uIseconds) $HN $SCRIPT INFO: CASSANDRA 2.0 container entrypoint st
 # check if pql has ben initlizized
 if [ ! -f /opt/cassandra-data/CELL_CASS_INIT.txt ]; then
 	echo "InitDB cell casandra DB"
-	/opt/cassandra_init.sh
+	#/opt/cassandra_init.sh
 fi
 # As this container uses host network we include the name of the host and IP in host for name resulution.
 # this is requied for cassandra's gossip startup
@@ -25,13 +25,35 @@ export CELL_EXEC_SLEEP=$WAIT  # wait 5 seconds to start it
 
 trap "DO_LOOP=false; killall -q java ; killall -q confd" HUP INT QUIT KILL TERM EXIT
 
+rm -f /opt/cassandra_pid.pid # Remove pid file if exisits
+
 while $DO_LOOP; do
-	/opt/cell_entry.sh /opt/cassandra/bin/cassandra -f -p /opt/cassandra_pid.pid
-	if [ -f "/opt/cassandra_restart.txt" ] ; then
-		DO_LOOP=true
-		rm -f /opt/cassandra_restart.txt
+	if [ -e "/opt/cassandra/casscell.pid" ] ; then
+		# server is possible running
+		ps $(cat /opt/cassandra/casscell.pid) > /dev/null
+		if [ $? -eq 0 ] ; then
+			if [ -e "/opt/cassandra_restart.txt" ] ; then
+				echo "Stoping cassandra..."
+				rm -f /opt/cassandra_restart.txt
+				kill $(cat /opt/cassandra/casscell.pid)
+				killall -q java
+			else
+				sleep 90
+			fi
+		else
+			#remove dirty pid file
+			echo "WARN: Remove dirty PID"
+			rm -f /opt/cassandra/casscell.pid
+		fi
 	else
-		DO_LOOP=false
+		# server is not running -> do start it
+		echo "Cassandra process not found... starting cassandra...."
+		killall -q java
+		/opt/cell_entry.sh /opt/cassandra/bin/cassandra -p /opt/cassandra/casscell.pid
+		sleep 90
+		rm -f /opt/cassandra_restart.txt # Clean restart on init
+		echo "Cassandra process started with PID=$(cat /opt/cassandra/casscell.pid)"
 	fi
+	sleep 5 # Give some time
 done
 echo "$(date -uIseconds) $HN $SCRIPT INFO: CASSANDRA 2.0 container has been stopped"
