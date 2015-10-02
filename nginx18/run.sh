4b5bd206fe12f63590980a49193e278682e2e29a
ludiazv <ldiaz@atloideas.com>
2015-10-02 16:29:20 +0200

    fix nginx

--- nginx18/run.sh
+++ nginx18/run.sh
@@ -2,4 +2,4 @@
 source /etc/environment
 PREFIX="/"
 docker run -i --rm -t -e HOST_IP=${COREOS_PRIVATE_IPV4} -e CELL_ETCD_PREFIX=${PREFIX} \
-		-p ${COREOS_PRIVATE_IPV4}:80:80 -p ${COREOS_PRIVATE_IPV4}:443:443 --privileged --name nginx atlo/nginx16
+		-p ${COREOS_PRIVATE_IPV4}:80:80 -p ${COREOS_PRIVATE_IPV4}:443:443 --privileged --name nginx atlo/nginx
