d489951380467b69d116e3f8390d882ff25676d8
ludiazv <ldiaz@atloideas.com>
2016-03-15 15:51:21 +0100

    Cell alpine version

--- cell/build.sh
+++ cell/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 VERSION=1.0
-QUIET=--quiet
+QUIET=
 docker build $QUIET --force-rm=true --rm=true --tag="atlo/cell:$VERSION" .
 docker tag -f atlo/cell:$VERSION atlo/cell:latest
+docker build $QUIER --force-rm=true --rm=true --tag="atlo/cell-alpine:$VERSION" -f Dockerfile.alpine .
+docker tag -f atlo/cell-alpine:$VERSION atlo/cell-alpine:latest
