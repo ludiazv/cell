0b6fde8fb3b5387a372a143a16555716d492c547
ludiazv <ldiaz@atloideas.com>
2016-06-07 16:45:31 +0200

    push all repositories

--- push_all.sh
+++ push_all.sh
@@ -3,8 +3,10 @@ docker login
 docker push atlo/cell
 docker push atlo/cell-alpine
 docker push atlo/redis28
+docker push atlo/redis
 docker push atlo/psql94
 docker push atlo/ruby2
+docker push atlo/ruby2-alpine
 docker push atlo/jruby9k
 docker push atlo/rails42
 docker push atlo/golang14
@@ -12,6 +14,7 @@ docker push atlo/mysql
 docker push atlo/memcached
 docker push atlo/jre7
 docker push atlo/cassandra20
+docker push atlo/registry:1.0
 docker push atlo/registry
 docker push atlo/nginx
 docker push atlo/wordpress
