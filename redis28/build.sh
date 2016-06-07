82e7294098a9961828dd8363dfdb73eb34310214
ludiazv <ldiaz@atloideas.com>
2016-06-07 16:46:07 +0200

    reds image based on alpine

--- redis28/build.sh
+++ redis28/build.sh
@@ -3,3 +3,5 @@ VERSION=1.0
 QUIET=--quiet
 docker build $QUIET --force-rm=true --rm=true --tag="atlo/redis28:$VERSION" .
 docker tag -f atlo/redis28:$VERSION atlo/redis28:latest
+docker build $QUIET --force-rm=true --rm=true --tag="atlo/redis:$VERSION" -f Dockerfile.alpine .
+docker tag -f atlo/redis:$VERSION atlo/redis:latest
