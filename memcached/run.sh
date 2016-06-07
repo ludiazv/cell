720af62ebf27a0f0dbf8b89bbb0e7c88f7e1faa8
ludiazv <ldiaz@atloideas.com>
2016-06-07 18:17:46 +0200

    fix memcached run

--- memcached/run.sh
+++ memcached/run.sh
@@ -3,5 +3,5 @@ source /etc/environment
 PREFIX="/"
 docker run -i --rm -t --name memcached \
 	   -e MC_SIZE=20 -e MC_CONN=256 \
-	   -p ${COREOS_PRIVATE_IPV4}:11211:11211 
+	   -p ${COREOS_PRIVATE_IPV4}:11211:11211 \
 	   atlo/memcached:latest
