a19c6cafa51ddc12daf07ae5dd7bfafc63526dcc
ludiazv <ldiaz@atloideas.com>
2016-06-07 16:45:03 +0200

    update jruby

--- jruby9k/build.sh
+++ jruby9k/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-VERSION=9.0.1.0
-QUIET=--quiet
+VERSION=9.0.5.0
+QUIET=
 docker build $QUIET --force-rm=true --rm=true --tag="atlo/jruby9k:$VERSION" .
 docker tag -f atlo/jruby9k:$VERSION atlo/jruby9k:latest
+docker build $QUIET --force-rm=true --rm=true --tag="atlo/jruby9k-alpine:$VERSION" -f Dockerfile.alpine .
+docker tag -f atlo/jruby9k-alpine:$VERSION atlo/jruby9k-alpine:latest
