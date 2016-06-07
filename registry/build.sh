e5874dd75ad6134b019c9848369781164a72814c
ludiazv <ldiaz@atloideas.com>
2016-06-07 16:46:47 +0200

    add docker registry v2

--- registry/build.sh
+++ registry/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 VERSION=1.0
+VERSIONTWO=2.0
 QUIET=--quiet
 docker build $QUIET --force-rm=true --rm=true --tag="atlo/registry:$VERSION" .
-docker tag -f atlo/registry:$VERSION atlo/registry:latest
+#docker tag -f atlo/registry:$VERSION atlo/registry:latest
+docker build $QUIET --force-rm=true --rm=true --tag="atlo/registry:$VERSIONTWO" -f Dockerfile.v2 .
+docker tag -f atlo/registry:$VERSIONTWO atlo/registry:latest
--- registry/run.sh
+++ registry/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 source /etc/environment
-docker run -i --rm -t -p ${COREOS_PRIVATE_IPV4}:5000:5000 --name registro -v $(pwd)/test-data:/opt/registry-data atlo/registry
+#docker run -i --rm -t -p ${COREOS_PRIVATE_IPV4}:5000:5000 --name registro -v $(pwd)/test-data:/opt/registry-data atlo/registry
+docker run -i -t --rm -p ${COREOS_PRIVATE_IPV4}:5000:5000 --name registro \
+      -v $(pwd)/test-data:/opt/registry-data atlo/registry:2.0
